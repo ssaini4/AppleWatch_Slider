175db964d1015fda9d6a79a313f56fb56646ff3e
ssaini4 <ssaini4@illinois.edu>
2015-07-23 21:42:23 -0500

    Initial Commit

---
+++ Slider_Switch_application/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Slider_Switch_application
+//
+//  Created by Saksham Saini on 7/23/15.
+//  Copyright (c) 2015 Saksham Saini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Slider_Switch_applicationTests/Slider_Switch_applicationTests.m
@@ -0,0 +1,40 @@
+//
+//  Slider_Switch_applicationTests.m
+//  Slider_Switch_applicationTests
+//
+//  Created by Saksham Saini on 7/23/15.
+//  Copyright (c) 2015 Saksham Saini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Slider_Switch_applicationTests : XCTestCase
+
+@end
+
+@implementation Slider_Switch_applicationTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
