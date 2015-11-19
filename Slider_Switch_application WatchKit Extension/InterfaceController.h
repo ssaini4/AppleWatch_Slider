c2df9ac5bdd94814a5acc58597fb9849ccdd66eb
ssaini4 <ssaini4@illinois.edu>
2015-11-19 15:59:14 -0600

    done

---
+++ Slider_Switch_application WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  Slider_Switch_application WatchKit Extension
+//
+//  Created by Saksham Saini on 7/23/15.
+//  Copyright (c) 2015 Saksham Saini. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
---
+++ Slider_Switch_application WatchKit Extension/InterfaceController.m
@@ -0,0 +1,38 @@
+//
+//  InterfaceController.m
+//  Slider_Switch_application WatchKit Extension
+//
+//  Created by Saksham Saini on 7/23/15.
+//  Copyright (c) 2015 Saksham Saini. All rights reserved.
+//
+
+#import "InterfaceController.h"
+
+
+@interface InterfaceController()
+
+@end
+
+
+@implementation InterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
